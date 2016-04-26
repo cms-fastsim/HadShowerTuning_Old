1ff738b62ccf084f58b5cf36bdb408e69888ef9b
Lukas <lukas.vanelderen@cern.ch>
2016-04-26 10:00:25 +0200

    get g4 installation script to work on lxplus

--- installGeant4.sh
+++ installGeant4.sh
@@ -1,5 +1,6 @@
 mypwd=$PWD
-G4VERSION="10.02.p01"
+G4VERSION="10.01.p02"
+wget http://geant4.cern.ch/support/source/geant4.$G4VERSION.tar.gz
 tar -xzf geant4.$G4VERSION.tar.gz
 mkdir geant4.$G4VERSION-build
 cd geant4.$G4VERSION-build
@@ -8,8 +9,8 @@ cmake \
 -DGEANT4_USE_OPENGL_X11=ON \
 -DGEANT4_INSTALL_DATA=ON \
 -DGEANT4_USE_GDML=ON \
--DXERCESC_LIBRARY=$CMSSW_RELEASE_BASE/../../../external/xerces-c/2.8.0/lib/libxerces-c.so \
--DXERCESC_INCLUDE_DIR=$CMSSW_RELEASE_BASE/../../../external/xerces-c/2.8.0/include/ \
+-DXERCESC_LIBRARY=$CMSSW_RELEASE_BASE/../../../external/xerces-c/2.8.0-cms/lib/libxerces-c.so \
+-DXERCESC_INCLUDE_DIR=$CMSSW_RELEASE_BASE/../../../external/xerces-c/2.8.0-cms/include/ \
 -DGEANT4_BUILD_CXXSTD=c++11 \
 $mypwd/geant4.$G4VERSION
 make -j 12
